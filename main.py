be9af5376229e00a7c74b6c17c87b50caa6b5fc4
Roshan Senevirathne <roshaaanrulz@gmail.com>
2024-02-12 09:13:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import algorithm
+from app.model.model import __version__ as model_version
+from typing import List
+import numpy as np
+
+app = FastAPI()
+
+
+class TextIn(BaseModel):
+    company_persona: dict
+    candidate_array: List[dict]
+    w_soft_skills: float
+    w_technical_skills: float
+    w_education: float
+    w_experience: float
+
+
+class PredictionOut(BaseModel):
+    prediction: List[dict]
+
+
+@app.get("/")
+def home():
+    return {"health_check": "OK", "model_version": model_version}
+
+
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    prediction = algorithm(payload.company_persona,payload.candidate_array,payload.w_technical_skills,payload.w_education,payload.w_soft_skills,payload.w_experience)
+    return {"prediction": prediction}
---
+++ model/model.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from sklearn.metrics.pairwise import cosine_similarity
+
+preprocess_url='https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+model_url='https://tfhub.dev/google/electra_base/2'
+bert_preprocess_model=hub.KerasLayer(preprocess_url)
+bert_encode_model = hub.KerasLayer(model_url)
+
+def get_sentence_embeddings(sentences):
+  text_preprocessed = bert_preprocess_model(sentences)
+  return bert_encode_model(text_preprocessed)['pooled_output']
+
+def get_similarity(vectors):
+  count=vectors.get_shape()[0]
+  results=[]
+  for i in range (1,count):
+    results.append(cosine_similarity([vectors[0]],[vectors[i]])[0][0])
+  return results
+
+
+def algorithm(companyPersona,candidateArray,w_technical_skills,w_education,w_soft_skills,w_experience):
+    educationVector=[]
+    technicalSkillsVector=[]
+    softSkillsVector=[]
+    experienceVector=[]
+    educationVector.append(companyPersona['education'])
+    technicalSkillsVector.append(companyPersona['technical_skills'])
+    softSkillsVector.append(companyPersona['soft_skills'])
+    experienceVector.append(companyPersona['experience'])
+
+    for i in range(0,len(candidateArray)):
+        educationVector.append(candidateArray[i]['education'])
+        technicalSkillsVector.append(candidateArray[i]['technical_skills'])
+        softSkillsVector.append(candidateArray[i]['soft_skills'])
+        experienceVector.append(candidateArray[i]['experience'])
+    
+    education=get_sentence_embeddings(educationVector)
+    technicalSkills=get_sentence_embeddings(technicalSkillsVector)
+    softSkills=get_sentence_embeddings(softSkillsVector)
+    experience=get_sentence_embeddings(experienceVector)
+
+    technicalSkillSet = get_similarity(technicalSkills)
+    educationSet= get_similarity(education)
+    softSkillSet= get_similarity(softSkills)
+    minimumExperienceSet = get_similarity(softSkills)
+
+    results=[]
+    for i in range(0,len(candidateArray)):
+        results.append((w_technical_skills*technicalSkillSet[i]+w_education*educationSet[i]+w_soft_skills*softSkillSet[i]+w_experience*minimumExperienceSet[i])*100)
+
+    return results
